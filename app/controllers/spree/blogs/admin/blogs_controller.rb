757f3cd73a46cf46350e6e7d3b4440f2f1a59807
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-08-28 17:09:10 +0200

    Upgrade layout to match new backend, metasearch -> ransack

--- app/controllers/spree/blogs/admin/blogs_controller.rb
+++ app/controllers/spree/blogs/admin/blogs_controller.rb
@@ -3,18 +3,18 @@ class Spree::Blogs::Admin::BlogsController < Spree::Admin::ResourceController
   def show
     redirect_to admin_blogs_path
   end
-  
+
 private
-  
+
   def find_resource
     Spree::Blog.find_by_permalink!(params[:id])
-  end  
-    
+  end
+
   def collection
-    params[:search] ||= {}
-    params[:search][:meta_sort] ||= "name.asc"
-    @search = Spree::Blog.metasearch(params[:search])
-    @collection = @search.page(params[:page]).per(Spree::Config[:orders_per_page])
+    params[:q] ||= {}
+    params[:q][:meta_sort] ||= "name.asc"
+    @search = Spree::Blog.search(params[:q])
+    @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
   end
 
 end
--- app/controllers/spree/blogs/admin/posts_controller.rb
+++ app/controllers/spree/blogs/admin/posts_controller.rb
@@ -1,37 +1,37 @@
 class Spree::Blogs::Admin::PostsController < Spree::Admin::ResourceController
-      
+
   def index
     @pages = collection
   end
-  
+
   def new
     @post = Spree::Post.new
     @post.posted_at ||= Time.now
   end
 
   private
-    
+
     update.before :set_category_ids
-    
+
     def set_category_ids
       if params[:post] && params[:post][:post_category_ids].is_a?(Array)
         params[:post][:post_category_ids].reject!{|i| i.to_i == 0 }
       end
     end
-    
+
     def location_after_save
       path = params[:redirect_to].to_s.strip.sub(/^\/+/, "/")
       path.blank? ? object_url : path
-    end 
-    
+    end
+
     def find_resource
 	  	@object ||= Spree::Post.find_by_path(params[:id])
     end
-    
+
     def collection
-      params[:search] ||= {}
-      params[:search][:meta_sort] ||= "posted_at.desc"
-      @search = Spree::Post.metasearch(params[:search])
+      params[:q] ||= {}
+      params[:q][:meta_sort] ||= "posted_at.desc"
+      @search = Spree::Post.ransack(params[:q])
       @collection = @search.page(params[:page]).per(Spree::Post.per_page)
     end
 
