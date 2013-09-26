b6a916645fc073fe3a96bb9b552d6e71c1fc3099
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-09-26 13:25:00 +0200

    Make name and permalink avaiable for mass assignment

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -1,16 +1,17 @@
 class Spree::Blog < ActiveRecord::Base
-  
   RESERVED_PATHS = /(^\/*(admin|account|cart|checkout|content|login|pg|orders|products|s|session|signup|shipments|states|t|tax_categories|user)\/+)/
-  
+
   has_many :posts, :class_name => "Spree::Post", :dependent => :destroy
   has_many :categories, :through => :posts, :source => :post_categories, :uniq => true
-  
+
   validates :name, :presence => true
   validates :permalink, :uniqueness => true, :format => { :with => /^[a-z0-9\-\_\/]+$/i }, :length => { :within => 3..40 }
   validate  :permalink_availablity
-  
+
   before_validation :normalize_permalink
-  
+
+  attr_accessible :name, :permalink
+
   def self.find_by_permalink!(path)
     super path.to_s.gsub(/(^\/+)|(\/+$)/, "")
   end
@@ -18,15 +19,15 @@ class Spree::Blog < ActiveRecord::Base
   def self.find_by_permalink(path)
     find_by_permalink!(path) rescue ActiveRecord::RecordNotFound
   end
-  
+
   def self.to_options
     self.order(:name).map{|i| [ i.name, i.id ] }
   end
-  
+
   def to_param
     self.permalink.gsub(/(^\/+)|(\/+$)/, "")
   end
-  
+
 private
 
   def permalink_availablity
@@ -36,5 +37,5 @@ private
   def normalize_permalink
     self.permalink = (permalink.blank? ? name.to_s.parameterize : permalink).downcase.gsub(/(^[\/\-\_]+)|([\/\-\_]+$)/, "")
   end
-  
+
 end
